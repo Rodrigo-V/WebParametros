ebc1ed680a036f2b35b0016af3d153349c992e85
RODRIGO VILCHEZ FERNANDEZ <rvilchezf@outlook.com>
2019-03-11 04:37:49 -0300

    Primer commit

---
+++ WebParametros/default.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="default.aspx.vb" Inherits="WebParametros._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Consulta con parametros</title>
+</head>
+<body>
+    <h1>Consulta con parametros</h1>
+    <form id="form1" runat="server">
+        <div>
+
+
+
+
+
+            <asp:Label ID="Label1" runat="server" Text="Codigo de Cliente"></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+             <asp:Label ID="Label2" runat="server" Text="Nombre"></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+             <asp:Label ID="Label3" runat="server" Text="Direccion"></asp:Label>
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br />
+            <br />
+             <asp:Label ID="Label4" runat="server" Text="Codigo de distrito"></asp:Label>
+            <asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList>
+            
+            <br />
+            <br />
+             <asp:Label ID="Label5" runat="server" Text="Sexo"></asp:Label>
+            <asp:DropDownList ID="DropDownList2" runat="server"></asp:DropDownList>
+            <br />
+            <br />
+             <asp:Label ID="Label6" runat="server" Text="DNI"></asp:Label>
+            <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+            <br />
+            <br />
+             <asp:Label ID="Label7" runat="server" Text="Ruc"></asp:Label>
+            <asp:TextBox ID="TextBox7" runat="server"></asp:TextBox>
+            <br />
+            <br />
+             <asp:Label ID="Label8" runat="server" Text="Telefono"></asp:Label>
+            <asp:TextBox ID="TextBox8" runat="server"></asp:TextBox>
+            <br />
+            <br />
+             <asp:Label ID="Label9" runat="server" Text="Celular"></asp:Label>
+            <asp:TextBox ID="TextBox9" runat="server"></asp:TextBox>
+
+
+
+
+
+
+            <br />
+            <br />
+            <br />
+
+
+
+
+
+
+        </div>
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+    </form>
+</body>
+</html>
---
+++ WebParametros/default.aspx.vb
@@ -0,0 +1,95 @@
+﻿Imports System.Data.SqlClient
+Public Class _default
+    Inherits System.Web.UI.Page
+    Dim cmd As New SqlCommand
+    Dim conn As New SqlConnection("Data Source=LAPTOP-GUIQPSQR\SQLEXPRESS;Initial Catalog=Farmacia;Integrated Security=True")
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        LlenaCombo()
+        llenacombo2()
+
+    End Sub
+
+    Protected Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Public Sub LlenaCombo()
+
+
+        Try
+            If conn.State = ConnectionState.Closed Then
+                cmd.Connection = conn
+                conn.Open()
+            End If
+            cmd.CommandType = Data.CommandType.StoredProcedure
+            cmd.CommandText = "sp_tres"
+
+            Dim DS As New DataSet
+            Dim da As New SqlDataAdapter(cmd)
+
+            da.Fill(DS)
+
+            DropDownList1.DataSource = DS
+            DropDownList1.DataTextField = "nom_dis"
+            DropDownList1.DataValueField = "cod_dis"
+            DropDownList1.DataBind()
+
+
+
+        Catch ex As Exception
+
+        Finally
+            If conn.State = ConnectionState.Open Then
+                cmd.Connection = conn
+                conn.Close()
+            End If
+
+        End Try
+
+    End Sub
+    Private Sub llenacombo2()
+
+        DropDownList2.Items.Add("M")
+        DropDownList2.Items.Add("F")
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            If conn.State = ConnectionState.Closed Then
+                cmd.Connection = conn
+                conn.Open()
+            End If
+            cmd.CommandType = Data.CommandType.StoredProcedure
+            cmd.CommandText = "sp_guardar"
+
+            cmd.Parameters.Add(New SqlParameter("@codigo", TextBox1.Text))
+            cmd.Parameters.Add(New SqlParameter("@nombre", TextBox2.Text))
+            cmd.Parameters.Add(New SqlParameter("@direccion", TextBox3.Text))
+            cmd.Parameters.Add(New SqlParameter("@cod_distrito", DropDownList1.SelectedValue))
+            cmd.Parameters.Add(New SqlParameter("@sexo", DropDownList2.SelectedValue))
+            cmd.Parameters.Add(New SqlParameter("@dni", TextBox6.Text))
+            cmd.Parameters.Add(New SqlParameter("@ruc", TextBox7.Text))
+            cmd.Parameters.Add(New SqlParameter("@telefono", TextBox8.Text))
+            cmd.Parameters.Add(New SqlParameter("@celular", TextBox9.Text))
+
+            If cmd.ExecuteNonQuery() Then
+                MsgBox("REGISTRO GUARADADO CORRECTAMENTE", vbExclamation)
+            Else
+                MsgBox("no se guardo el formulario" + vbCritical)
+            End If
+
+
+        Catch ex As Exception
+
+        Finally
+            If conn.State = ConnectionState.Open Then
+                cmd.Connection = conn
+                conn.Close()
+            End If
+
+        End Try
+
+
+    End Sub
+End Class
